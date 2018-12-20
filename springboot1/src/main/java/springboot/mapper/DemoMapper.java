91b3a72875451cf8fb02443545b8a2fe667d06df
xbl <282624695@qq.com>
2018-12-20 16:47:01 +0800

    删除测试数据

--- springboot1/src/main/java/springboot/mapper/DemoMapper.java
+++ springboot1/src/main/java/springboot/mapper/DemoMapper.java
@@ -5,9 +5,4 @@ import org.apache.ibatis.annotations.Select;
 public interface DemoMapper {
     @Select(value = "")
     int select();
-
-StringBuffer sb = new StringBuffer();
-    StringBuffer sb2= new StringBuffer();
-    StringBuffer sb3= new StringBuffer();
-    StringBuffer getSb3 new StringBuffer();
 }
