459e0e4424221cc8d35632f670c8d74e916862a3
xbl <282624695@qq.com>
2018-12-25 18:09:37 +0800

    mapperscan

--- scis-master/src/main/java/com/jcohy/scis/ScisApplication.java
+++ scis-master/src/main/java/com/jcohy/scis/ScisApplication.java
@@ -1,7 +1,6 @@
 package com.jcohy.scis;
 
 import com.jcohy.scis.interception.CommonIntercepter;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
